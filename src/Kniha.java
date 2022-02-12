588077f34b753ad94c71a0a13decd6f110791747
Marek Pástor <marekpastorml@gmail.com>
2022-02-12 22:34:21 +0100

    Vytvorenie Knihy

---
+++ src/Kniha.java
@@ -0,0 +1,11 @@
+public class Kniha {
+    public String nazov;
+    public String autor;
+    int pocet_stran;
+
+    public Kniha(String nazov, String autor, int pocet_stran) {
+        this.nazov = nazov;
+        this.autor = autor;
+        this.pocet_stran = pocet_stran;
+    }
+}
